e69ca0a71b56c21c1b5a3ac7d27123547bf34b42
LinhNV <vietlinhtspt@gmail.com>
2020-04-10 06:11:18 +0700

    Change index.js

--- index.js
+++ index.js
@@ -2,10 +2,13 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-import signUP from './authScreen/signUp/signUp';
-import signIn from './authScreen/signIn/signIn';
-import mainBody from './main/mainBody'
-AppRegistry.registerComponent(appName, () => mainBody);
+import { AppRegistry } from "react-native";
+import App from "./App";
+import { name as appName } from "./app.json";
+import signUP from "./authScreen/signUp/signUp";
+import signIn from "./authScreen/signIn/signIn";
+import verifyCode from "./authScreen/verifyCode/verifyCode";
+import recoveryPassStep1 from "./authScreen/recoveryPassStep1/recoveryPassStep1";
+import favouriteFood from "./main/favouriteFood/favouriteFood";
+import mainBody from "./main/mainBody";
+AppRegistry.registerComponent(appName, () => favouriteFood);
