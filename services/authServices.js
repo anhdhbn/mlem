63ef181a6b904ed8ea42a3992130cf57b53a084c
LinhNV <vietlinhtspt@gmail.com>
2020-04-15 19:35:20 +0700

    Fix issue in authServices.

--- services/authServices.js
+++ services/authServices.js
@@ -15,8 +15,8 @@ export default {
   forgotPassword,
   verifyCode,
   recoveryPass,
-  getUserInfoByUsername,
-  updateUserInfoByUsername,
-  logout,
+  // getUserInfoByUsername,
+  // updateUserInfoByUsername,
+  // logout,
   createUser
 };
