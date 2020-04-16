eaf33bd00244199fae7a8a328d53cdfe4658fc41
LinhNV <vietlinhtspt@gmail.com>
2020-04-16 18:45:49 +0700

    Update Tabbar with library React-navigation

--- pages/mainBody.js
+++ pages/mainBody.js
@@ -1,15 +1,15 @@
 import React, { useEffect, useState, Component } from "react";
-import { View, Text, SafeAreaView, StatusBar } from "react-native";
-import ScrollableTabView, {
-  DefaultTabBar,
-} from "react-native-scrollable-tab-view";
+// import { View, Text, SafeAreaView, StatusBar } from "react-native";
+// import ScrollableTabView, {
+//   DefaultTabBar,
+// } from "react-native-scrollable-tab-view";
 
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-
-import styles from "../styles/homeStyle";
-import TabBar from "../components/tabBar/tabBar";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import Icon from "react-native-vector-icons/FontAwesome";
+// import styles from "../styles/homeStyle";
+// import TabBar from "../components/tabBar/tabBar";
 import HomePage from "./home";
-console.disableYellowBox = true;
+// console.disableYellowBox = true;
 
 const Tab = createMaterialTopTabNavigator();
 
@@ -39,13 +39,58 @@ export default class MainBody extends Component {
       //   </ScrollableTabView>
       // </SafeAreaView>
 
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomePage} />
-        <Tab.Screen name="Order" component={HomePage} />
-        <Tab.Screen name="Diary" component={HomePage} />
-        <Tab.Screen name="Settings" component={HomePage} />
+      <Tab.Navigator
+        initialRouteName="Home"
+        tabBarPosition="bottom"
+        lazy="true"
+        tabBarOptions={{
+          activeTintColor: "#DF0000",
+          inactiveTintColor: "grey",
+          labelStyle: { fontSize: 12, textTransform: "capitalize" },
+          showIcon: true,
+        }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={HomePage}
+          options={{
+            tabBarLabel: "Trang chủ".toLocaleLowerCase(),
+            tabBarIcon: ({ _, color }) => (
+              <Icon name="home" color={color} size={24} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Order"
+          component={HomePage}
+          options={{
+            tabBarLabel: "Đặt bàn",
+            tabBarIcon: ({ focused, color }) => (
+              <Icon name="group" color={color} size={24} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Diary"
+          component={HomePage}
+          options={{
+            tabBarLabel: "Nhật ký",
+            tabBarIcon: ({ _, color }) => (
+              <Icon name="address-book" color={color} size={24} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={HomePage}
+          options={{
+            tabBarLabel: "Tôi",
+            tabBarIcon: ({ _, color }) => (
+              <Icon name="user" color={color} size={24} />
+            ),
+          }}
+        />
       </Tab.Navigator>
-
     );
   }
 }
