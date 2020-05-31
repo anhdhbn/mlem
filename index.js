ccd7472d723a99769e54facf012fd58ee9a343fb
hungvm <hungvimanh.cntt@gmail.com>
2020-05-31 17:36:05 +0700

    fix load App

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ import { AppRegistry } from "react-native";
 import App from "./App";
 import { name as appName } from "./app.json";
 import AddNewFood from "./providers/Menu/AddNewFood";
-AppRegistry.registerComponent(appName, () => AddNewFood);
+AppRegistry.registerComponent(appName, () => App);
