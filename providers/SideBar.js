0eeb7dd7f65f628bcccb5436d9d740321b8988ce
datdinh <madatk56>
2020-06-22 23:14:05 +0700

    del table page

--- providers/SideBar.js
+++ providers/SideBar.js
@@ -3,7 +3,6 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import OrderStackScreen from "./Order/Order";
 import MenuStackScreen from "./Menu/Menu";
-import TableStackScreen from "./Tables/Table";
 import NotificationStackScreen from "./Notification/Notification";
 import StatisticStackScreen from './Statistics/Statistics';
 import PaymentStackScreen from './Payment/Payment';
@@ -13,7 +12,6 @@ export default function () {
     <drawer.Navigator initialRouteName="Home">
       <drawer.Screen name="Home" component={OrderStackScreen} />
       <drawer.Screen name="Menu" component={MenuStackScreen} />
-      <drawer.Screen name="Table" component={TableStackScreen} />
       <drawer.Screen name="Payment" component={PaymentStackScreen} />
       <drawer.Screen name="Notification" component={NotificationStackScreen} />
       <drawer.Screen name="Statistic" component= {StatisticStackScreen} />
